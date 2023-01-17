c0a01f62dca821b93dd1deff55190a0a90258a52
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-17 18:50:58 +0100

    feat: finished

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Association Manager Angular Front-end</h1>
+<h4 align="center">Mael KERICHARD (@Pixselve) - Romain BRIEND (@Yami2200)</h4>
+<p align="center">
+   <img src="https://img.shields.io/badge/-ESIR-orange" alt="ESIR">
+   <img src="https://img.shields.io/badge/-Android-blue" alt="Android">
+   <img src="https://img.shields.io/badge/-Kotlin-red" alt="Kotlin">
+</p>
+
+---
+
+En se basant sur les informations du cours, puis :
+https://developer.android.com/guide/topics/ui/declaring-layout
+
+1. Créer une interface Linear layout avec un un texte à gauche, puis un bouton à droite
+2. Créer une interface Linear layout avec un texte au dessus, puis un bouton en
+   dessous
+3. Créer une interface à bas de Linear layout avec un label au dessus, puis en dessous
+   un texte remplissable à gauche et un bouton à droite
+4. Faire la même chose qu'à la question précédente avec un Relative Layout
+5. Créer une interface avec une list View qui affichera le contenu d’une arraylist que
+   vous aurez rempli auparavant avec le nom des 4 départements Breton
+6. Même exercice qu’à la question précédente mais ajouter à la fin de l’affichage de
+   chacun des noms de département le suffixe “ - Bretagne”
+7. Enfin faites en sorte que l’on puisse cliquer sur chacun des éléments de la liste et
+   affiche une popup avec le Nom du département et sa population qui sera stocker
+   dans une autre arrayList ou une map.
--- app/src/main/java/com/example/tp2_prog_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tp2_prog_mobile/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.tp2_prog_mobile
 
 import android.os.Bundle
+import android.os.Message
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tp2_prog_mobile.ui.theme.Tp2progmobileTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +32,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tp2progmobileTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -31,10 +48,94 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun textLeftButtonRight() {
+    Row {
+        Text(text = "I'm a text")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "This is a Button")
+        }
+    }
+}
+
+@Composable
+fun textTopButtonBottom() {
+    Column {
+        Text(text = "I'm a text")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "This is a Button")
+        }
+    }
+}
+
+@Composable
+fun question3() {
+    Column {
+        Text(text = "I'm a label")
+        Row {
+            TextField(value = "", onValueChange = {})
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "This is a Button")
+            }
+        }
+    }
+}
+
+@Composable
+fun question5(departements: List<String>) {
+    LazyColumn() {
+        items(departements) { name ->
+            Text(text = name)
+        }
+    }
+}
+
+@Composable
+fun question6(departements: List<String>) {
+    LazyColumn {
+        items(departements) { name ->
+            Text(text = "$name - Bretagne")
+        }
+    }
+}
+
+@Composable
+fun SingleDepartement(departement: String, population: Int) {
+    val showPopup = remember { mutableStateOf(false) }
+    Text(text = departement, modifier = Modifier.clickable {
+        showPopup.value = true
+    })
+    if (showPopup.value) {
+        AlertDialog(
+            onDismissRequest = { showPopup.value = false },
+            icon = {
+                Icon(Icons.Filled.Person, contentDescription = null)
+            },
+            title = { Text(text = "Population") },
+            text = { Text(text = "La population du département $departement est de $population habitants") },
+            confirmButton = {
+                Button(onClick = { showPopup.value = false }) {
+                    Text(text = "OK")
+                }
+            }
+        )
+    }
+}
+@Composable
+fun question7(departements: List<String>, population: HashMap<String, Int>) {
+    LazyColumn {
+        items(departements) { name ->
+            SingleDepartement(departement = name, population = population[name]!!)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val departements = listOf("Côtes-d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan")
+    val population = hashMapOf("Côtes-d'Armor" to 650000, "Finistère" to 1000000, "Ille-et-Vilaine" to 1500000, "Morbihan" to 800000)
     Tp2progmobileTheme {
-        Greeting("Android")
+        question7(departements, population)
     }
 }
